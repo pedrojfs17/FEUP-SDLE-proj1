15baab10c9ed022f0946d021cce8e503b7e25c4c
pedrojfs17 <up201806227@fe.up.pt>
2021-11-22 14:47:04 +0000

    Server saving status

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -4,8 +4,14 @@ use std::collections::VecDeque;
 use serde_json;
 use std::fs::File;
 use scheduled_thread_pool;
-// use std::thread;
 use std::time;
+#[macro_use] extern crate lazy_static;
+use std::sync::Mutex;
+
+lazy_static!(
+    static ref TOPICS: Mutex<HashMap<String, HashMap<String, VecDeque<String>>>> = Mutex::new(HashMap::new());
+    static ref PENDING_REQUESTS: Mutex<HashMap<String, HashSet<String>>> = Mutex::new(HashMap::new());
+);
 
 fn main() {
     let context = zmq::Context::new();
@@ -16,36 +22,63 @@ fn main() {
         .bind("tcp://*:5559")
         .expect("failed binding socket");
 
-    let mut topics: HashMap<String, HashMap<String, VecDeque<String>>> = HashMap::new();
-    let mut pending_requests: HashMap<String, HashSet<String>> = HashMap::new();
-
-    let file = File::create("state").unwrap();
+    recover_state();
 
     let scheduled_thread_pool = scheduled_thread_pool::ScheduledThreadPool::new(1);
 
-    scheduled_thread_pool.execute_at_fixed_rate(time::Duration::from_secs(5), time::Duration::from_secs(5), move || {
-        routine_save(&file, &topics) }
-    );
+    scheduled_thread_pool.execute_at_fixed_rate(time::Duration::from_secs(5), time::Duration::from_secs(5), || {
+        let topics_file: File = File::create("topics.json").unwrap();
+        serde_json::to_writer(&topics_file, &*TOPICS).unwrap();
+
+        let pendings_file: File = File::create("pending.json").unwrap();
+        serde_json::to_writer(&pendings_file, &*PENDING_REQUESTS).unwrap();
+    });
 
     loop {
         let (request_id, request) = read_message(&socket);
 
-        parse_request(&request_id, request, &socket, &mut topics, &mut pending_requests);
+        parse_request(&request_id, request, &socket);
 
-        for (topic, map) in &topics {
-            println!("Topic: {}", topic);
-            for (id, deq) in map {
-                println!("\t{} : {:?}", id, deq);
-            }
-        }
+        print_topics();
     }
 }
 
-fn routine_save(file: &File, topics: &HashMap<String, HashMap<String, VecDeque<String>>>) {
-    serde_json::to_writer(file, topics);
+fn print_topics() {
+    println!("TOPICS");
+    for (topic, map) in TOPICS.lock().unwrap().iter() {
+        println!("{}:", topic);
+        for (id, deq) in map {
+            println!("\t{} : {:?}", id, deq);
+        }
+    }
+    println!();
 }
 
+fn print_pending_requests() {
+    println!("PENDING REQUESTS");
+    for (topic, set) in PENDING_REQUESTS.lock().unwrap().iter() {
+        println!("{} : {:?}", topic, set);
+    }
+    println!();
+}
 
+fn recover_state() {
+    let file = File::open("topics.json");
+
+    if file.is_ok() {
+        *TOPICS.lock().unwrap() = serde_json::from_reader(file.unwrap()).unwrap();
+
+        print_topics();
+    }
+
+    let file = File::open("pending.json");
+
+    if file.is_ok() {
+        *PENDING_REQUESTS.lock().unwrap() = serde_json::from_reader(file.unwrap()).unwrap();
+
+        print_pending_requests();
+    }
+}
 
 fn read_message(socket: &zmq::Socket) -> (String, String) {
     let request_id = socket
@@ -69,7 +102,7 @@ fn read_message(socket: &zmq::Socket) -> (String, String) {
 fn send_message(socket: &zmq::Socket, id: &String, message: &str) -> i32 {
     let id_part_result = socket.send(id.as_bytes(), zmq::SNDMORE);
     match id_part_result {
-        Ok(v) => println!("Result OK: {:?}", v),
+        Ok(_v) => {},
         Err(e) => {
             println!("Result Error: {:?}", e);
             return -1;
@@ -78,7 +111,7 @@ fn send_message(socket: &zmq::Socket, id: &String, message: &str) -> i32 {
 
     let empty_part_result = socket.send("", zmq::SNDMORE);
     match empty_part_result {
-        Ok(v) => println!("Result OK: {:?}", v),
+        Ok(_v) => {},
         Err(e) => {
             println!("Result Error: {:?}", e);
             return -1;
@@ -87,7 +120,7 @@ fn send_message(socket: &zmq::Socket, id: &String, message: &str) -> i32 {
 
     let message_result = socket.send(message, 0);
     match message_result {
-        Ok(v) => println!("Result OK: {:?}", v),
+        Ok(_v) => {},
         Err(e) => {
             println!("Result Error: {:?}", e);
             return -1;
@@ -97,7 +130,9 @@ fn send_message(socket: &zmq::Socket, id: &String, message: &str) -> i32 {
     return 0;
 }
 
-fn parse_request(request_id: &String, request: String, socket: &zmq::Socket, topics: &mut HashMap<String, HashMap<String, VecDeque<String>>>, pending_requests: &mut HashMap<String, HashSet<String>>) {
+fn parse_request(request_id: &String, request: String, socket: &zmq::Socket) {
+    let mut topics = TOPICS.lock().unwrap();
+
     let split: Vec<_> = request.splitn(2, " ").collect();
 
     let action = split[0];
@@ -145,7 +180,7 @@ fn parse_request(request_id: &String, request: String, socket: &zmq::Socket, top
                 if topic_map.contains_key(request_id) {
                     let value = topic_map.get_mut(request_id).unwrap().pop_front();
                     if value == None {
-                        add_pending_request(pending_requests, String::from(topic), String::from(request_id));
+                        add_pending_request(String::from(topic), String::from(request_id));
                     } else {
                         send_message(&socket, &request_id, value.unwrap().as_str());
                     }
@@ -173,7 +208,9 @@ fn parse_request(request_id: &String, request: String, socket: &zmq::Socket, top
 
             send_message(&socket, &request_id, "OK");
 
-            check_pending_requests(String::from(topic), socket, topics, pending_requests);
+            drop(topics);
+
+            check_pending_requests(String::from(topic), socket);
         },
 
         _ => {
@@ -183,17 +220,22 @@ fn parse_request(request_id: &String, request: String, socket: &zmq::Socket, top
 
 }
 
-fn add_pending_request(map: &mut HashMap<String, HashSet<String>>, topic: String, id: String) {
-    if map.contains_key(&topic) {
-        map.get_mut(&topic).unwrap().insert(id);
+fn add_pending_request(topic: String, id: String) {
+    let mut pending_requests = PENDING_REQUESTS.lock().unwrap();
+
+    if pending_requests.contains_key(&topic) {
+        pending_requests.get_mut(&topic).unwrap().insert(id);
     } else {
         let mut set: HashSet<String> = HashSet::new();
         set.insert(id);
-        map.insert(topic, set);
+        pending_requests.insert(topic, set);
     }
 }
 
-fn check_pending_requests(topic: String, socket: &zmq::Socket, topics: &mut HashMap<String, HashMap<String, VecDeque<String>>>, pending_requests: &mut HashMap<String, HashSet<String>>) {
+fn check_pending_requests(topic: String, socket: &zmq::Socket) {
+    let mut topics = TOPICS.lock().unwrap();
+    let mut pending_requests = PENDING_REQUESTS.lock().unwrap();
+
     if pending_requests.contains_key(&topic) {
         let requests = pending_requests.get_mut(&topic).unwrap();
         let topic_map = topics.get_mut(&topic).unwrap();
